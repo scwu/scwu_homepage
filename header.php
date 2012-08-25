7cc9797c408cf224e26f53ec8e6e0aaa4c6b968a
Clara Wu <s.clara.wu@gmail.com>
2012-08-24 21:30:19 -0400

    changed the font

--- header.php
+++ header.php
@@ -6,6 +6,7 @@ echo'
 	<meta name="author" content="Clara Wu">
 	<title>Shin-Yuan Clara Wu</title>
 	<link rel="stylesheet" href="css/style.css" type="text/css" />
+	<link href="http://fonts.googleapis.com/css?family=Adamina" rel= "stylesheet" type= "text/css">
 	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
 	<script type="text/javascript" src="js/slimbox2.js"></script> 
 	<link rel="stylesheet" href="css/slimbox2.css" type="text/css" media="screen" /> <!-- href path should be changed to where you have installed your slimbox2 css file-->
